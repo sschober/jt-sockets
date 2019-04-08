1906d685f488b9c421a2593e45c64d8bfbbe2322
Sven Schober <sschober@sssm.de>
2019-04-08 09:32:35 +0200

    JtDatagramPacket - create factory method

--- src/main/java/de/sssm/jt/raw/socket/JtDatagramPacket.java
+++ src/main/java/de/sssm/jt/raw/socket/JtDatagramPacket.java
@@ -5,8 +5,12 @@
  */
 package de.sssm.jt.raw.socket;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Data object representing a datagram packet
+ * 
  * @author sven
  */
 public class JtDatagramPacket {
@@ -16,11 +20,38 @@ public class JtDatagramPacket {
     public final String destAddress;
     public final int destPort;
 
-    public JtDatagramPacket(byte[] bytes, String sourceAddress, int sourcePort, String destAddress, int destPort) {
+    private JtDatagramPacket(byte[] bytes, String sourceAddress, int sourcePort, String destAddress, int destPort) {
         this.bytes = bytes;
         this.sourceAddress = sourceAddress;
         this.sourcePort = sourcePort;
         this.destAddress = destAddress;
         this.destPort = destPort;
-    }  
+    }
+
+    /**
+     * Constructs a {@link JtDatagramPacket} from the arguments given.
+     * <p/>
+     * Special consideration to the address parts is given: the
+     * address might be passed as a hostname, or as a numerical IP
+     * address, the first form is tried to resolve to the numerical
+     * form.
+     * <p/>
+     * The resulting packet will always contain addresses in
+     * numerical form. 
+     */
+    public static JtDatagramPacket of(byte[] bytes, String sourceAddress, int sourcePort, String destAddress,
+            int destPort) throws UnknownHostException {
+        InetAddress sourceInetAddress = null;
+        if(!sourceAddress.isEmpty()){
+            sourceInetAddress = InetAddress.getByName(sourceAddress);
+        }
+        else {
+            sourceInetAddress = InetAddress.getLocalHost();
+        }
+        InetAddress targetInetAddress = null;
+        if(!destAddress.isEmpty()){
+            targetInetAddress = InetAddress.getByName(destAddress);
+        }
+        return new JtDatagramPacket(bytes, sourceInetAddress.getHostAddress(), sourcePort, targetInetAddress.getHostAddress(), destPort);
+    }
 }
